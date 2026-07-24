dacca35d862122c20743bfe7f13d4dfc87640ef5
melbryan147 <49510790+melbryan147@users.noreply.github.com>
2026-07-24 11:54:37 +0800

    CRUD for User
    added more functionality for CRUD opeartion for super admin and admin

--- server/routes/userManagement.js
+++ server/routes/userManagement.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const { toggleUserStatus } = require('../controllers/userManagementController');
+const { toggleUserStatus, insertUser,updateUser,deleteUser, getUserById, getAllUsers} = require('../controllers/userManagementController');
 const router = express.Router();
 
 router.post('/toggle', toggleUserStatus);
+router.post('/insert', insertUser);
+router.get('/get/:userId', getUserById);
+router.get('/all', getAllUsers);
+router.put('/update/:userId', updateUser);
+router.delete('/delete/:userId', deleteUser);
 
 module.exports = router;
