8893706421b7447faa9ad9db289d4b4622e8c2b5
melbryan147 <49510790+melbryan147@users.noreply.github.com>
2026-07-29 12:54:48 +0800

    increase timeout

--- server/db.js
+++ server/db.js
@@ -12,7 +12,7 @@ module.exports.pool = mysql.createPool({
   password:  process.env.DB_PASS,
   port: Number(process.env.DB_PORT), 
   waitForConnections: true,
-  connectTimeout: 10000,   // increase timeout to 10s
+  connectTimeout: 20000,   // increase timeout to 10s
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
